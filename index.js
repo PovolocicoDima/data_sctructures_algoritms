2e460aff5023321c1a7e4d6333af3bc6de2ec1d5
Поволочко Дмитрий <povolochko@reg.ru>
2023-07-17 21:27:27 +0300

    second

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-test
+function addUpToN(n) {
+  let result = 0;
+
+  for (let i = 0; i <= n; i += 1) {
+    result += i;
+  }
+
+  return result;
+}
+
+function secondAddUpToN(n) {
+  return (n * (n + 1)) / 2;
+}
+
+console.log(secondAddUpToN(101000000000));
