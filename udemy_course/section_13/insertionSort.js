b9276e31c98c9133ee3bcf27d9120092214931f1
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2024-01-12 15:27:43 +0200

    insertion sort added

---
+++ udemy_course/section_13/insertionSort.js
@@ -0,0 +1,38 @@
+// function insertionSort(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     let currentItem = arr[i];
+//     if (currentItem < arr[i - 1]) {
+//       let searchedIdx = i - 1;
+//       for (let j = i - 1; j >= 0; j--) {
+//         if (arr[j] > currentItem) {
+//           searchedIdx--;
+//         }
+//       }
+
+//       arr.splice(searchedIdx, 0, currentItem);
+//     }
+//   }
+
+//   return arr;
+// }
+
+// console.log(insertionSort([3, 44, 38, 5, 47, 15]));
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentValue = arr[i];
+    let j;
+    for (j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
+      console.log(arr);
+      console.log(arr[i], arr[j]);
+      console.log(arr[j + 1]);
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentValue;
+    console.log("`````");
+  }
+
+  return arr;
+}
+
+console.log(insertionSort([4, 1, 0, 2, -1]));
