9aa2ef796b124396a7242c460fe36054aa0ea0f5
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2024-02-16 12:37:14 +0200

    added pop method to singly linked list

--- udemy_course/section_19_singly_linked_lists/list.js
+++ udemy_course/section_19_singly_linked_lists/list.js
@@ -24,14 +24,52 @@ class SinglyLinkedList {
         this.length += 1
         return this
     }
-}
 
+    pop() {
+        if (this.length === 0) {
+            return undefined
+        } else if (this.length === 1) {
+            const temp = this.head
+            this.head = null
+            this.tail = null
+            this.length = 0
+            return temp
+        } else {
+            let temp = null
+            let current = this.head
+            for (let i = 0; i < this.length; i++) {
+                if (i === this.length - 2) {
+                    temp = current.next
+                    current.next = null
+                    this.tail = current
+                    this.length -= 1
+                    break;
+                }
+                current = current.next
+            }
+            return temp
+        }
+    }
+
+    traverse() {
+        let current = this.head
+        while(current) {
+            console.log(current)
+            current = current.next
+        }
+    }
+}
 
 const list = new SinglyLinkedList()
 list.push('How')
 list.push('are')
 list.push('you')
 // console.log(list)
-console.log(list.head)
-console.log(list.head.next)
-console.log(list.tail)
+// console.log(list.head)
+// console.log(list.head.next)
+// console.log(list.tail)
+
+console.log(list.pop())
+console.log(list.pop())
+console.log(list.pop())
+console.log(list)
