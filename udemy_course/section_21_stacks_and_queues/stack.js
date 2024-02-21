7d8538e6d4eaeb092e25c0526ab7f4e21cb36e90
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2024-02-21 14:55:12 +0200

    section 21: added pop method to stack

--- udemy_course/section_21_stacks_and_queues/stack.js
+++ udemy_course/section_21_stacks_and_queues/stack.js
@@ -2,7 +2,9 @@
     Last in first out (LIFO)
     Последний зашел - первый вышел.
     Аналогия - стопка листов бумаги: 
-    поставил лист сверху и потом его-же и взял
+    один за другим ставим листы в стопку, какой лист будет
+    поставлен в стопку последним - тот лист первым из этой
+    стопки и возьмут
 
     Stack - это концепт и он может быть реализован по разному
 */
@@ -35,10 +37,21 @@ class Stack {
         this.size += 1
         return this.size
     }
+
+    pop() {
+        if (this.size === 0) return null
+        if (this.first === this.last) this.last = null
+        
+        const node = this.first
+        this.first = this.first.next
+        this.size -= 1
+        return node.val
+    }
 }
 
 const stack = new Stack()
 stack.push(1)
 stack.push(2)
-// stack.push(3)
+stack.push(3)
+stack.pop()
 console.log(stack)
