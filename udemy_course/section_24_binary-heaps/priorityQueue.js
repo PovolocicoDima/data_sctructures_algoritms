096574ba4effbfd7d5176bfd912e04033cb5c1c0
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2024-03-12 15:07:48 +0200

    section_25 added prettier get and set methods into hashTables

--- udemy_course/section_24_binary-heaps/priorityQueue.js
+++ udemy_course/section_24_binary-heaps/priorityQueue.js
@@ -1,78 +1,79 @@
 class Node {
-    constructor(val, priority) {
-        this.val = val
-        this.priority = priority
-    }
+  constructor(val, priority) {
+    this.val = val
+    this.priority = priority
+  }
 }
 
 class PriorityQueue {
-    constructor() {
-        this.values = []
+  constructor() {
+    this.values = []
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1
+    const element = this.values[idx]
+
+    while (idx > 0) {
+      let parentIndex = Math.floor((idx - 1) / 2)
+      let parent = this.values[parentIndex]
+      if (element.priority >= parent.priority) break
+      this.values[parentIndex] = element
+      this.values[idx] = parent
+      idx = parentIndex
     }
+  }
 
-    bubbleUp() {
-        let idx = this.values.length - 1
-        const element = this.values[idx]
+  cascadeDown() {
+    let idx = 0
+    let length = this.values.length
+    let element = this.values[0]
 
-        while(idx > 0) {
-            let parentIndex = Math.floor((idx - 1) / 2)
-            let parent = this.values[parentIndex]
-            if (element.priority >= parent.priority) break;
-            this.values[parentIndex] = element
-            this.values[idx] = parent
-            idx = parentIndex
+    while (true) {
+      let leftIdx = 2 * idx + 1
+      let rightIdx = 2 * idx + 2
+      let leftChild, rightChild
+      let swapIdx = null
+
+      if (leftIdx < length) {
+        leftChild = this.values[leftIdx]
+        if (leftChild.priority < element.priority) {
+          swapIdx = leftIdx
         }
-    }
+      }
 
-    cascadeDown() {
-        let idx = 0
-        let length = this.values.length
-        let element = this.values[0]
-
-        while(true) {
-            let leftIdx = 2 * idx + 1
-            let rightIdx = 2 * idx + 2
-            let leftChild, rightChild
-            let swapIdx = null
-
-            if (leftIdx < length) {
-                leftChild = this.values[leftIdx]
-                if (leftChild.priority < element.priority) {
-                    swapIdx = leftIdx
-                }
-            }
-
-            if (rightIdx < length) {
-                rightChild = this.values[rightIdx]
-                if ((swapIdx === null && rightChild.priority < element.priority) || 
-                    (swapIdx !== null && rightChild.priority < leftChild.priority)) 
-                {
-                    swapIdx = rightIdx
-                }
-            }
-
-            if (swapIdx === null) break;
-            this.values[idx] = this.values[swapIdx]
-            this.values[swapIdx] = element
-            idx = swapIdx
+      if (rightIdx < length) {
+        rightChild = this.values[rightIdx]
+        if (
+          (swapIdx === null && rightChild.priority < element.priority) ||
+          (swapIdx !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swapIdx = rightIdx
         }
-    }
+      }
 
-    enqueue(value, priority) {
-        this.values.push(new Node(value, priority))
-        this.bubbleUp()
-        return this.values
+      if (swapIdx === null) break
+      this.values[idx] = this.values[swapIdx]
+      this.values[swapIdx] = element
+      idx = swapIdx
     }
+  }
 
-    dequeue() {
-        const min = this.values[0]
-        const end = this.values.pop()
-        if (this.values.length > 0) {
-          this.values[0] = end
-          this.cascadeDown()
-        }
-        return min
+  enqueue(value, priority) {
+    this.values.push(new Node(value, priority))
+    this.bubbleUp()
+    return this.values
+  }
+
+  dequeue() {
+    const min = this.values[0]
+    const end = this.values.pop()
+    if (this.values.length > 0) {
+      this.values[0] = end
+      this.cascadeDown()
     }
+    return min
+  }
 }
 
 const priorityQueue = new PriorityQueue()
