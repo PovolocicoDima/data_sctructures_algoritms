659a704ce0e51f7142c6be339450dd26fdb5cf8a
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2024-03-12 16:06:39 +0200

    section_25 added keys and values methods into hashTables

--- udemy_course/section_25_hash_tables/hashTable.js
+++ udemy_course/section_25_hash_tables/hashTable.js
@@ -1,3 +1,11 @@
+/**
+ * BIG O(average cases)
+ *
+ * ISERT:  O(1)
+ * DELETE: O(1)
+ * ACCESS: O(1)
+ */
+
 class HashTable {
   constructor(size = 53) {
     this.keyMap = new Array(size)
@@ -31,9 +39,28 @@ class HashTable {
     }
     return undefined
   }
+  _traversal(idx) {
+    let result = new Set()
+    this.keyMap.map((item) => {
+      if (item.length) {
+        return item.map((i) => {
+          return idx === 'keys' ? result.add(i[0]) : result.add(i[1])
+        })
+      }
+    })
+
+    return Array.from(result)
+  }
+  keys() {
+    return this._traversal('keys')
+  }
+  values() {
+    return this._traversal('values')
+  }
 }
 
 let ht = new HashTable(17)
+ht.set('maroon', '#800002')
 ht.set('maroon', '#800002')
 ht.set('yellow', '#FFFF00')
 ht.set('olive', '#808000')
@@ -41,4 +68,5 @@ ht.set('salmon', '#FA8072')
 ht.set('lightcoral', '#F08080')
 ht.set('mediumvioletred', '#C71585')
 ht.set('plum', '#DDA0DD')
-console.log(ht.get('maroon'))
+console.log(ht.values())
+console.log(ht.keys())
