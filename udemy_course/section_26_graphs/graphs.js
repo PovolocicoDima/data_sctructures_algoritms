c5fb56b96c9eb6722e44023e8d0957613d5150c7
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2024-03-15 17:07:42 +0200

    section 26: added graph with adjecency list and methdods: addvertex, addedge, removeedge, deleteVertex

---
+++ udemy_course/section_26_graphs/graphs.js
@@ -0,0 +1,77 @@
+/**
+ * Составляющие части графов:
+ * - vertex(вершина) - a node
+ * - edge(край, ребро) - a connection between nodes
+ * Типы графов:
+ * - weighted ( взвешанные )
+ * - unweighted ( не взвешанные )
+ * - directed ( направленные )
+ * - undirected ( ненаправленные )
+ * Ну и их комбинации типа weighted directed, unweighted undirected и так далее
+ */
+
+/**
+ * Виды хранения графов:
+ * - adjacency matrix ( марица смежности )
+ * - adjecency list ( листы смежности )
+ *     [
+ *      [1, 5], -> 0     - индексы массива указывают на числа node(vertes) а числа
+ *      [0, 2], -> 1       которые указаны в массиве по данном индексу указывают
+ *      [1, 3], -> 2       на ноды между которыми находится данная нода, т.е. :
+ *      [2, 4], -> 3       число 0 находится в графе между числами 1 и 5
+ *      [3, 5], -> 4       число 1 находится между числами 0 и 2
+ *      [4, 0], -> 5       число 2 находится между числами 1 и 3 и так далее(в примере учитывается
+ *     ]                    что граф ненаправленный)
+ *
+ */
+
+// UNDIRECTED GRAPH
+
+class Graph {
+  constructor() {
+    this.adjecencyList = {}
+  }
+
+  addVertex(vertex) {
+    this.adjecencyList[vertex] = []
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjecencyList[vertex1].push(vertex2)
+    this.adjecencyList[vertex2].push(vertex1)
+    return this.adjecencyList
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjecencyList[vertex1] = this.adjecencyList[vertex1].filter(
+      (i) => i !== vertex2
+    )
+    this.adjecencyList[vertex2] = this.adjecencyList[vertex2].filter(
+      (i) => i !== vertex1
+    )
+  }
+
+  deleteVertex(vertex) {
+    delete this.adjecencyList[vertex]
+    const keys = Object.keys(this.adjecencyList)
+    for (let i = 0; i < keys.length; i++) {
+      if (this.adjecencyList[keys[i]].includes(vertex))
+        this.adjecencyList[keys[i]] = this.adjecencyList[keys[i]].filter(
+          (i) => i !== vertex
+        )
+    }
+  }
+}
+
+const graph = new Graph()
+graph.addVertex('Moskow')
+graph.addVertex('Tokyo')
+graph.addVertex('Dallas')
+graph.addVertex('Chisinau')
+graph.addEdge('Chisinau', 'Tokyo')
+graph.addEdge('Chisinau', 'Dallas')
+graph.addEdge('Dallas', 'Moskow')
+graph.addEdge('Tokyo', 'Dallas')
+console.log(graph.adjecencyList)
+graph.deleteVertex('Chisinau')
+console.log(graph.adjecencyList)
