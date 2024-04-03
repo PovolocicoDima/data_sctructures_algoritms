fc97856400d48fab7cc2ad9df43a271198dae678
Povolocico Dmitrii <dmitrii.povolocico@ourbox.org>
2024-04-03 15:57:26 +0300

    added duplicates and add word tasks

---
+++ udemy_course/section_30_course_tasks/findDuplicates.js
@@ -0,0 +1,32 @@
+/**
+ * Frequency Counter - findAllDuplicates
+
+Given an array of positive integers, some elements 
+appear twice and others appear once. 
+Find all the elements that appear twice in this array. 
+Note that you can return the elements in any order.
+ */
+
+/**
+ *     findAllDuplicates([4,3,2,7,8,2,3,1])         // array with 2 and 3
+       findAllDuplicates([4, 3, 2, 1, 0])           // []
+       findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3]) // array with 3, 2, and 1
+ */
+
+function findAllDuplicates(arr) {
+  // add whatever parameters you deem necessary - good luck!
+  const hash = {}
+  const result = []
+  for (let i = 0; i < arr.length; i++) {
+    let currentItem = arr[i]
+    if (!hash[currentItem]) {
+      hash[currentItem] = 1
+    } else {
+      result.push(currentItem)
+    }
+  }
+
+  return result
+}
+
+console.log(findAllDuplicates([4, 3, 2, 7, 8, 2, 3, 1]))
